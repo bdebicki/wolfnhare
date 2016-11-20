8851e0d0d78b3dcfe90361db6888c01f7aaa2f71
Earl Debicki <bartosz.debicki@uxpin.com>
2016-11-21 00:27:29 +0100

    handling basket position

--- src/js/Actions/Wolf.js
+++ src/js/Actions/Wolf.js
@@ -1,6 +1,6 @@
 WOLF = (function (scope) {
     scope.setWolfPosition = function (side) {
-        var wolfElement = document.getElementById(this.wolfBody.wolfId),
+        var wolfElement = document.getElementById(scope.wolfBody.wolfId),
             wolfSide = this.wolfBody[side];
 
         if (!wolfElement.classList.contains(wolfSide)) {
@@ -9,7 +9,20 @@ WOLF = (function (scope) {
         } else {
             return null;
         }
-    }
+    };
+
+    scope.setBasketPosition = function (position) {
+        var basketElement = document.getElementsByClassName(scope.basket.basketClass)[0],
+            basketPosition = this.basket[position];
+        console.log(basketElement);
+
+        if (!basketElement.classList.contains(basketPosition)) {
+            basketElement.classList.remove(this.basket.onTopLeft, this.basket.onTopRight, this.basket.onBottomLeft, this.basket.onBottomRight);
+            basketElement.classList.add(basketPosition);
+        } else {
+            return null;
+        }
+    };
 
     return scope;
 })(WOLF);
