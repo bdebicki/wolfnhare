281b7182db7b28bf1afdabc1d1ba8d91adf6db7e
Earl Debicki <bartosz.debicki@uxpin.com>
2016-11-23 00:48:32 +0100

    add js builder + minified js builder

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
     autoprefixer = require('gulp-autoprefixer'),
-    minify = require('gulp-minify-css');
+    minify = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
 
 // build css file task
 gulp.task('less', function() {
@@ -19,6 +21,27 @@ gulp.task('less:minify', function() {
         .pipe(autoprefixer({ browsers: ['last 2 versions', 'ie >= 10'] }))
         .pipe(minify())
         .pipe(gulp.dest('./public/css/'));
+});
+
+// build js file task
+gulp.task('js', function() {
+    return gulp.src(['./src/js/characters/wolf.js',
+                     './src/js/actions/wolf.js',
+                     './src/js/components/wolfNavigation.js',
+                     './src/js/helpers/wolfMoveEvents.js'])
+        .pipe(concat('wolfnhare.js'))
+        .pipe(gulp.dest('./public/js/'));
+});
+
+// build minified js file task
+gulp.task('js:minify', function() {
+    return gulp.src(['./src/js/characters/wolf.js',
+                     './src/js/actions/wolf.js',
+                     './src/js/components/wolfNavigation.js',
+                     './src/js/helpers/wolfMoveEvents.js'])
+        .pipe(concat('wolfnhare.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/js/'));
 });
 
 // css watch task
