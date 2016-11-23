2c119e825964ef65e3c93528e9fa3ce5195b145c
Earl Debicki <bartosz.debicki@uxpin.com>
2016-11-24 00:42:47 +0100

    first concept of chickens characters

---
+++ src/js/characters/chickens.js
@@ -0,0 +1,15 @@
+var CHICKENS = {
+    chicken: {
+        chickenClass: 'chicken',
+        position: {
+            topLeft: 'topLeft',
+            bottomLeft: 'bottomLeft',
+            topRight: 'topRight',
+            bottomRight: 'bottomRight'
+        }
+    },
+
+    egg: {
+        steps: 5
+    }
+};
