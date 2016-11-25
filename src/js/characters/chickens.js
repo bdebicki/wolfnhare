1c1cf1f546ebabbdfda3a19e8f0ea5c7c1ec9c17
Earl Debicki <bartosz.debicki@uxpin.com>
2016-11-25 01:47:20 +0100

    add chickens to chicken object

--- src/js/characters/chickens.js
+++ src/js/characters/chickens.js
@@ -1,15 +1,40 @@
 var CHICKENS = {
     chicken: {
         chickenClass: 'chicken',
-        position: {
-            topLeft: 'topLeft',
-            bottomLeft: 'bottomLeft',
-            topRight: 'topRight',
-            bottomRight: 'bottomRight'
+        defaultEgg: false,
+
+        chickenTrigger: function(position) {
+            for (var i = 0, chicken = document.getElementsByClassName(this.chickenClass); i<chicken.length; i++) {
+                if (chicken[i].classList.contains(position)) {
+                    return chicken[i];
+                }
+            }
+        },
+
+        first: {
+            position: 'topLeft',
+            eggState: function() { return CHICKENS.chicken.defaultEgg },
+            chickenDomEl: function() { return CHICKENS.chicken.chickenTrigger(this.position) }
+        },
+        second: {
+            position: 'bottomLeft',
+            eggState: function() { return CHICKENS.chicken.defaultEgg },
+            chickenDomEl: function() { return CHICKENS.chicken.chickenTrigger(this.position) }
+        },
+        third: {
+            position: 'topRight',
+            eggState: function() { return CHICKENS.chicken.defaultEgg },
+            chickenDomEl: function() { return CHICKENS.chicken.chickenTrigger(this.position) }
+        },
+        fourth: {
+            position: 'bottomRight',
+            eggState: function() { return CHICKENS.chicken.defaultEgg },
+            chickenDomEl: function() { return CHICKENS.chicken.chickenTrigger(this.position) }
         }
     },
 
     egg: {
-        steps: 5
+        steps: 5,
+        defaultStep: 1
     }
 };
