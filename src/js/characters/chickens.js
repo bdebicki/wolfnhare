95fabce75b603d6564dfb11086546f76483d55b6
Earl Debicki <bartosz.debicki@uxpin.com>
2016-11-27 20:46:23 +0100

    remove specific chicken objects and add getChicken method

--- src/js/characters/chickens.js
+++ src/js/characters/chickens.js
@@ -1,40 +1,15 @@
-var CHICKENS = {
-    chicken: {
-        chickenClass: 'chicken',
-        defaultEgg: false,
+var CHICKEN = {
+    chickenClass: 'chicken',
+    firstChicken: 'topLeft',
+    secondChicken: 'bottomLeft',
+    thirdChicken: 'topRight',
+    fourthChicken: 'bottomRight',
 
-        chickenTrigger: function(position) {
-            for (var i = 0, chicken = document.getElementsByClassName(this.chickenClass); i<chicken.length; i++) {
-                if (chicken[i].classList.contains(position)) {
-                    return chicken[i];
-                }
+    getChicken: function(position) {
+        for (var i = 0, chicken = document.getElementsByClassName(this.chickenClass); i<chicken.length; i++) {
+            if (chicken[i].classList.contains(position)) {
+                return chicken[i];
             }
-        },
-
-        first: {
-            position: 'topLeft',
-            eggState: function() { return CHICKENS.chicken.defaultEgg },
-            chickenDomEl: function() { return CHICKENS.chicken.chickenTrigger(this.position) }
-        },
-        second: {
-            position: 'bottomLeft',
-            eggState: function() { return CHICKENS.chicken.defaultEgg },
-            chickenDomEl: function() { return CHICKENS.chicken.chickenTrigger(this.position) }
-        },
-        third: {
-            position: 'topRight',
-            eggState: function() { return CHICKENS.chicken.defaultEgg },
-            chickenDomEl: function() { return CHICKENS.chicken.chickenTrigger(this.position) }
-        },
-        fourth: {
-            position: 'bottomRight',
-            eggState: function() { return CHICKENS.chicken.defaultEgg },
-            chickenDomEl: function() { return CHICKENS.chicken.chickenTrigger(this.position) }
         }
-    },
-
-    egg: {
-        steps: 5,
-        defaultStep: 1
     }
 };
