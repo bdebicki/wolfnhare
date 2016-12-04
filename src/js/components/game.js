e20039bdfd5f265ebd4783e0b88346220c8009f5
Earl Debicki <bartosz.debicki@uxpin.com>
2016-12-04 22:38:21 +0100

    add default score

--- src/js/components/game.js
+++ src/js/components/game.js
@@ -1,11 +1,30 @@
 var GAME = {
+    startScore: 0,
     roundTime: function () {
         return EGGS.stepTime * EGGS.maxStep;
     },
     btn: function(btnType) {
         return document.querySelector('button[data-game="' + btnType + '"]')
     },
-    startBtn: function() {return this.btn('start')},
-    pauseBtn: function() {return this.btn('pause')},
-    restartBtn: function() {return this.btn('restart')}
+    startBtn: function() {
+        return this.btn('start')
+    },
+    pauseBtn: function() {
+        return this.btn('pause')
+    },
+    restartBtn: function() {
+        return this.btn('restart')
+    },
+
+    scoreContainer: function() {
+        return document.querySelector('.score');
+    },
+    updateScore: function(score) {
+        this.scoreContainer().innerHTML = score;
+    },
+    renderDefaultScore: function() {
+        this.updateScore(this.startScore);
+    }
 };
+
+GAME.renderDefaultScore();
