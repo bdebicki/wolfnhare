7d6c6daf9223694078fb9a4a8da5d2e484381cf3
Earl Debicki <bartosz.debicki@uxpin.com>
2016-12-14 00:17:57 +0100

    handle adding bonus points

--- src/js/components/gameScore.js
+++ src/js/components/gameScore.js
@@ -1,7 +1,11 @@
 GAME = (function (scope) {
+    scope.startScore = 0;
     scope.currentScore = null;
     scope.doublePoints = false;
-    scope.startScore = 0;
+    scope.bonusCriteria = {
+        points: 500,
+        lifes: 0
+    };
 
     scope.getScoreContainer = function() {
         return document.querySelector('.score');
@@ -10,11 +14,18 @@ GAME = (function (scope) {
         var points = this.doublePoints ? 2 : 1,
             newScore = score + points;
 
+        if (newScore === scope.bonusCriteria.points && WOLF.usedLifes === scope.bonusCriteria.lifes) {
+            scope.isBonusAvailable(true);
+        }
+
         scope.currentScore = newScore;
         scope.getScoreContainer().innerHTML = newScore;
     };
     scope.renderDefaultScore = function() {
         scope.getScoreContainer().innerHTML = scope.startScore;
+    };
+    scope.isBonusAvailable = function(boolen) {
+        scope.doublePoints = boolen;
     };
 
     return scope;
--- src/js/components/wolfLifes.js
+++ src/js/components/wolfLifes.js
@@ -1,16 +1,21 @@
 WOLF = (function (scope) {
-    var usedLifes = 0,
-        lifes = document.querySelectorAll('.lifes li'),
+    scope.usedLifes = 0;
+
+    var lifes = document.querySelectorAll('.lifes li'),
         lifesLimit = lifes.length;
 
     scope.updateLives = function () {
-        usedLifes++;
+        this.usedLifes = this.usedLifes + 1;
+
+        if (this.usedLifes === GAME.bonusCriteria.lifes + 1) {
+            GAME.isBonusAvailable(false);
+        }
 
         for (var i = 0, l = lifesLimit; i < l; i++) {
             if (!lifes[i].classList.contains('used')) {
                 lifes[i].classList.add('used');
 
-                if (usedLifes !== lifesLimit) {
+                if (this.usedLifes !== lifesLimit) {
                     return false;
                 } else {
                     setTimeout(function () {
