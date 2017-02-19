60a411ed572b02f62fb2523602d48d7027c5d86d
Earl Debicki <bartosz.debicki@uxpin.com>
2017-02-19 01:30:47 +0100

    add clock to demo

--- src/js/components/clock.js
+++ src/js/components/clock.js
@@ -7,6 +7,8 @@ var CLOCK = {
         clock.classList.add(this.clockClass);
 
         GAME.getGameBody().appendChild(clock);
+
+        setInterval(function() {CLOCK.updateTime()}, 200);
     },
   
     removeClock: function () {
@@ -17,5 +19,24 @@ var CLOCK = {
         } else {
             return null;
         }
+    },
+
+    updateTime: function () {
+        var time = new Date(),
+            hours = time.getHours(),
+            minutes = time.getMinutes(),
+            seconds = time.getSeconds();
+
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        document.querySelector('.' + CLOCK.clockClass).innerHTML = hours + ':' + minutes + ':' + seconds;
     }
 };
