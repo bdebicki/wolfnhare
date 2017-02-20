4d6a31c904913907f7b6f56beee92306cf229603
Earl Debicki <bartosz.debicki@uxpin.com>
2017-02-20 23:17:54 +0100

    move lifes classes to vars

--- src/js/components/wolfLifes.js
+++ src/js/components/wolfLifes.js
@@ -1,4 +1,7 @@
 WOLF = (function (scope) {
+    scope.lifesClass = 'lifes';
+    scope.usedLifeClass = 'used';
+    scope.halfLifeClass = 'half';
     scope.usedLifes = 0;
     scope.lifesLimit = 3;
 
@@ -13,7 +16,7 @@ WOLF = (function (scope) {
         var fragment = document.createDocumentFragment();
 
         var lifesContainer = document.createElement('ul');
-        lifesContainer.classList.add('lifes');
+        lifesContainer.classList.add(scope.lifesClass);
 
         for (var i=1; i <= this.lifesLimit; i++) {
             fragment.appendChild(renderSimpleLife(i));
@@ -28,8 +31,8 @@ WOLF = (function (scope) {
     };
 
     scope.updateLifes = function () {
-        var lifeUnused = document.querySelector('.lifes li:not(.used)'),
-            lifeUsedHalf = document.querySelector('.lifes li.half');
+        var lifeUnused = document.querySelector('.' + scope.lifesClass + ' li:not(.' + scope.usedLifeClass + ')'),
+            lifeUsedHalf = document.querySelector('.' + scope.lifesClass + ' li.' + scope.halfLifeClass);
 
         if (this.usedLifes !== GAME.bonusCriteria.lifes) {
             GAME.isBonusAvailable(false);
@@ -37,13 +40,13 @@ WOLF = (function (scope) {
 
         if (lifeUsedHalf) {
             updateUsedLifes(0.5);
-            lifeUsedHalf.classList.remove('half');
+            lifeUsedHalf.classList.remove(scope.halfLifeClass);
         } else if (lifeUnused && HARE.isHareVisible) {
             updateUsedLifes(0.5);
-            lifeUnused.classList.add('used', 'half');
+            lifeUnused.classList.add(scope.usedLifeClass, scope.halfLifeClass);
         } else if (lifeUnused && !HARE.isHareVisible) {
             updateUsedLifes(1);
-            lifeUnused.classList.add('used');
+            lifeUnused.classList.add(scope.usedLifeClass);
         }
 
         if (this.usedLifes === this.lifesLimit) {
@@ -54,17 +57,17 @@ WOLF = (function (scope) {
     };
 
     scope.resetLifes = function () {
-        var lifes = document.querySelectorAll('.lifes li');
+        var lifes = document.querySelectorAll('.' + scope.lifesClass + ' li');
         this.usedLifes = 0;
 
         for (var i = 0, l = lifes.length; i < l; i++) {
-            lifes[i].classList.remove('used');
+            lifes[i].classList.remove(scope.usedLifeClass);
         }
     };
 
     scope.removeLifes = function () {
         var game = GAME.getGameContainer(),
-            lifes = game.querySelector('.lifes');
+            lifes = game.querySelector('.' + scope.lifesClass);
 
         if(lifes) {
            game.removeChild(lifes);
